5b59bb760321505ee69629ba7e0ec97da171d554
bakemonostan <ehizomokhomion@gmail.com>
2022-10-22 09:35:48 -0700

    navbar styled, added shop and sign up links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Home from './routes/Home/Home';
+import Navbar from './components/navbar/Navbar';
 import { Routes, Route } from 'react-router-dom';
+import Shop from './routes/Shop/Shop';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navbar />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,29 @@
-const Navbar = () => {
-  return <div>Navbar</div>;
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+
+import './Navbar.styles.scss';
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <CrwnLogo className='logo' />
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+          <Link className='nav-link' to='/sign-in'>
+            SIGN IN
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
 };
-export default Navbar;
+
+export default Navigation;
