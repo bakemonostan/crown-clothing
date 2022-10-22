077c504a25102c5a6f266789b627d2c5a380f853
bakemonostan <ehizomokhomion@gmail.com>
2022-10-22 08:32:11 -0700

    first commit

---
+++ src/components/directory/Directory.jsx
@@ -0,0 +1,12 @@
+import CategoryItem from '../category-item/category-item';
+
+const Directory = ({ categories }) => {
+  return (
+    <div className='categories-container'>
+      {categories.map((category) => (
+        <CategoryItem category={category} key={category.id} />
+      ))}
+    </div>
+  );
+};
+export default Directory;
